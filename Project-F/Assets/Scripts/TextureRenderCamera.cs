2e24f06d7ed4ff0736f02952afb3ef1ed7b53663
DanT <danilatumanov@gmail.com>
2020-02-01 17:47:09 +0300

    Тест кастомных шейдеров

---
+++ Project-F/Assets/Scripts/TextureRenderCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureRenderCamera : MonoBehaviour
+{
+
+    private Camera _camera;
+
+
+    public RenderTexture Texture => _camera.activeTexture;
+    
+    private void Awake()
+    {
+        _camera = GetComponent<Camera>();
+        _camera.targetTexture = new RenderTexture(_camera.pixelWidth, _camera.pixelHeight, 0, RenderTextureFormat.ARGB32);
+    }
+    
+}
---
+++ Project-F/Assets/Scripts/WorldSwitchHideTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CapsuleCollider))]
+public class WorldSwitchHideTrigger : MonoBehaviour
+{
+
+    private CapsuleCollider _collider;
+
+    
+    private void Awake()
+    {
+        _collider = GetComponent<CapsuleCollider>();
+        _collider.radius = 0;
+    }
+
+
+    public void SetRadius(float radius)
+    {
+        _collider.radius = radius;
+    }
+
+}
